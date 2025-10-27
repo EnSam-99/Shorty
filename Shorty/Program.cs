a5ed0c2f7e104e90944bb5721782f8d569228d71
YelenaGhushchyan <yelghushchyan12@mail.ru>
2025-10-27 17:09:26 +0400

    Implement dal for Shorty

--- Shorty/Program.cs
+++ Shorty/Program.cs
@@ -1,7 +1,8 @@
 using Microsoft.OpenApi.Models;
 using Shorty.Components;
 using Shorty.Dal;
-
+using Shorty.Dal.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +17,7 @@ builder.Services.AddSwaggerGen(c =>
 
 builder.Services.AddSingleton<TestRepository>();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,7 +39,44 @@ app.UseSwaggerUI();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+app.MapPost("/api/shorty/create", async (CreateRequestModel request, AppDbContext db) =>
+{
+    
+    var user = await db.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+    if (user == null)
+    {
+        user = new User { Email = request.Email };
+        db.Users.Add(user);
+        await db.SaveChangesAsync();
+    }
+
+    
+    string shortCode;
+    do
+    {
+        shortCode = Guid.NewGuid().ToString().Substring(0, 6);
+    } while (await db.Shorties.AnyAsync(s => s.ShortUrl == shortCode));
+
+    var shorty = new Shorty.Dal.Models.Shorty()
+    {
+        Url = request.Url,
+        ShortUrl = shortCode,
+        CreatedAt = DateTime.UtcNow,
+        UserId = user.Id
+    };
+
+    db.Shorties.Add(shorty);
+    await db.SaveChangesAsync();
+
+    return Results.Ok(shorty);
+});
+
 app.MapControllers();
 
 app.Run();
 
+public class CreateRequestModel
+{
+    public string Email { get; set; }
+    public string Url { get; set; }
+}
